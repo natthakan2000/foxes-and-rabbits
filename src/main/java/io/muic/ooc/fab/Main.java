749717b2238f788ef3c0dda2ca15b898196f6ed3
Natthakan Euaumpon <natthakaneuaumpon@gmail.com>
2021-05-17 11:54:31 +0700

    changes

--- src/main/java/io/muic/ooc/fab/Main.java
+++ src/main/java/io/muic/ooc/fab/Main.java
@@ -3,7 +3,7 @@ package io.muic.ooc.fab;
 public class Main {
 
     public static void main(String[] args) {
-        var simulator = new Simulator();
+        Simulator simulator = new Simulator();
         simulator.simulate(1000);
     }
 }
--- src/main/java/io/muic/ooc/fab/Simulator.java
+++ src/main/java/io/muic/ooc/fab/Simulator.java
@@ -24,8 +24,9 @@ public class Simulator {
     private static final double RABBIT_CREATION_PROBABILITY = 0.08;
 
     // Lists of animals in the field.
-    private List<Rabbit> rabbits;
-    private List<Fox> foxes;
+//    private List<Rabbit> rabbits;
+//    private List<Fox> foxes;
+    private List<Animal> animals;
     // The current state of the field.
     private Field field;
     // The current step of the simulation.
@@ -56,8 +57,9 @@ public class Simulator {
             width = DEFAULT_WIDTH;
         }
 
-        rabbits = new ArrayList<>();
-        foxes = new ArrayList<>();
+//        rabbits = new ArrayList<>();
+//        foxes = new ArrayList<>();
+        animals = new ArrayList<>();
         field = new Field(depth, width);
 
         // Create a view of the state of each location in the field.
@@ -98,30 +100,38 @@ public class Simulator {
         step++;
 
         // Provide space for newborn rabbits.
-        List<Rabbit> newRabbits = new ArrayList<>();
+        List<Animal> newAnimals = new ArrayList<>();
         // Let all rabbits act.
-        for (Iterator<Rabbit> it = rabbits.iterator(); it.hasNext();) {
-            Rabbit rabbit = it.next();
-            rabbit.run(newRabbits);
-            if (!rabbit.isAlive()) {
-                it.remove();
-            }
-        }
-
-        // Provide space for newborn foxes.
-        List<Fox> newFoxes = new ArrayList<>();
-        // Let all foxes act.
-        for (Iterator<Fox> it = foxes.iterator(); it.hasNext();) {
-            Fox fox = it.next();
-            fox.hunt(newFoxes);
-            if (!fox.isAlive()) {
+//        for (Iterator<Rabbit> it = rabbits.iterator(); it.hasNext();) {
+//            Rabbit rabbit = it.next();
+//            rabbit.run(newRabbits);
+//            if (!rabbit.isAlive()) {
+//                it.remove();
+//            }
+//        }
+//
+//        // Provide space for newborn foxes.
+//        List<Fox> newFoxes = new ArrayList<>();
+//        // Let all foxes act.
+//        for (Iterator<Fox> it = foxes.iterator(); it.hasNext();) {
+//            Fox fox = it.next();
+//            fox.hunt(newFoxes);
+//            if (!fox.isAlive()) {
+//                it.remove();
+//            }
+//        }
+        for (Iterator<Animal> it = animals.iterator(); it.hasNext();) {
+            Animal animal = (Animal) it.next();
+            animal.giveBirth(newAnimals);
+            if (!animal.isAlive()) {
                 it.remove();
             }
         }
 
         // Add the newly born foxes and rabbits to the main lists.
-        rabbits.addAll(newRabbits);
-        foxes.addAll(newFoxes);
+//        rabbits.addAll(newRabbits);
+//        foxes.addAll(newFoxes);
+        animals.addAll(newAnimals);
 
         view.showStatus(step, field);
     }
@@ -131,8 +141,9 @@ public class Simulator {
      */
     public void reset() {
         step = 0;
-        rabbits.clear();
-        foxes.clear();
+//        rabbits.clear();
+//        foxes.clear();
+        animals.clear();
         populate();
 
         // Show the starting state in the view.
@@ -150,11 +161,11 @@ public class Simulator {
                 if (RANDOM.nextDouble() <= FOX_CREATION_PROBABILITY) {
                     Location location = new Location(row, col);
                     Fox fox = new Fox(true, field, location);
-                    foxes.add(fox);
+                    animals.add(fox);
                 } else if (RANDOM.nextDouble() <= RABBIT_CREATION_PROBABILITY) {
                     Location location = new Location(row, col);
                     Rabbit rabbit = new Rabbit(true, field, location);
-                    rabbits.add(rabbit);
+                    animals.add(rabbit);
                 }
                 // else leave the location empty.
             }
